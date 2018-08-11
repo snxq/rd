66c87a8abe8a1e688fd32926821347ab70c6d75a
Yodark <snxq1995@gmail.com>
2018-08-12 01:45:17 +0800

    Modify models && Add to admin

--- Backend/RESTfulAPI/admin.py
+++ Backend/RESTfulAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from RESTfulAPI.models import *
+
 # Register your models here.
+
+admin.site.register(Picture)
+admin.site.register(Tag)
--- Backend/RESTfulAPI/models.py
+++ Backend/RESTfulAPI/models.py
@@ -1,13 +1,19 @@
+from uuid import uuid4
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Tag(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    created_time = models.DateTimeField(default=timezone.now)
     name = models.CharField(unique=True, max_length=128)
 
 
 class Picture(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    created_time = models.DateTimeField(default=timezone.now)
     name = models.CharField(default='', max_length=64)
     source = models.URLField(default='')
-    tag = models.ManyToManyField("Tag")
-
+    tags = models.ManyToManyField("Tag")
