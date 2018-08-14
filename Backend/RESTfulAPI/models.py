ffcd2ed86aebd30f0ec7b6ca032e34e3f5c4f46b
yodark <snxq1995@gmail.com>
2018-08-14 15:56:50 +0800

    Update models

--- Backend/RESTfulAPI/models.py
+++ Backend/RESTfulAPI/models.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 # Create your models here.
 
 class Tag(models.Model):
-    id = models.UUIDField(default=uuid4, primary_key=True)
+    id_uuid = models.UUIDField(default=uuid4)
     created_time = models.DateTimeField(default=timezone.now)
     name = models.CharField(unique=True, max_length=128)
 
 
 class Picture(models.Model):
-    id = models.UUIDField(default=uuid4, primary_key=True)
+    id_uuid = models.UUIDField(default=uuid4)
     created_time = models.DateTimeField(default=timezone.now)
     name = models.CharField(default='', max_length=64)
     source = models.URLField(default='')
