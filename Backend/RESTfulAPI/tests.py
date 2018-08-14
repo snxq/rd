33cc68c62183d4663016f3b5f6112f7f486a37f8
yodark <snxq1995@gmail.com>
2018-08-15 03:35:54 +0800

    Add Search API With Single Keyword

--- Backend/RESTfulAPI/tests.py
+++ Backend/RESTfulAPI/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from RESTfulAPI.models import Picture
 # Create your tests here.
+
+
+class TestSearch(TestCase):
+    def setUp(self):
+        self.pics = [
+            {
+                "name": "测试1",
+                "source": "https://test.com"
+            },
+            {
+                "name": "test1",
+                "source": "https://test2.com"
+            }
+        ]
+        for pic in self.pics:
+            Picture.objects.create(**pic)
+        self.api = '/api/v1/search/'
+
+    def test_base(self):
+        response = self.client.get(f"{self.api}?keyword=测试")
+        self.assertEqual(response.json()[0], self.pics[0])
--- Backend/RESTfulAPI/views.py
+++ Backend/RESTfulAPI/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+import json
+
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
 from django.views import View
-from django.http import JsonResponse
+
+from RESTfulAPI.models import Picture
+from utils import dictsub
+
 # Create your views here.
 
 class Search(View):
     def get(self, request):
-        return JsonResponse({'code': '200'})
+        keyword = request.GET['keyword']
+        pics = Picture.objects.filter(name__contains=keyword)
+
+        response = [
+            dictsub(model_to_dict(pic), ['name', 'source'])
+            for pic in Picture.objects.filter(name__contains=keyword)
+        ]
+        return HttpResponse(json.dumps(response), content_type="application/json")
