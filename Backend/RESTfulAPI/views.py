6fe543b3585644b514f8bd192da58fbe3501c76f
yodark <snxq1995@gmail.com>
2018-08-16 22:31:57 +0800

    Update Search

--- Backend/RESTfulAPI/views.py
+++ Backend/RESTfulAPI/views.py
@@ -1,26 +1,58 @@
 import json
 
+import jieba
+import pytesseract
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
 from django.views import View
+from PIL import Image
 
 from RESTfulAPI.models import Picture
 from utils import dictsub
 
+import jieba
+from scipy.spatial.distance import sqeuclidean
 # Create your views here.
 
+
 class Search(View):
+    """Search API"""
     def get(self, request):
-        keyword = request.GET['keyword']
-        pics = Picture.objects.filter(name__contains=keyword)
+        """search逻辑
+        """
+        def serializer(queryset):
+            """序列化"""
+            response = []
+            for pic in queryset[:-1]:
+                pic_info = dictsub(model_to_dict(pic[0]), ['name', 'source', 'description'])
+                pic_info['tags'] = pic[2]
+                response.append(pic_info)
 
-        # 按照名字查找, 返回结果携带tags
-        response = []
-        for pic in pics:
+            return json.dumps(response)
+
+        # 参数处理
+        keywords = request.GET['keywords']
+        limit = max(int(request.GET['limit']), 100)
+        offset = int(request.GET['offset'])
+        words = list(jieba.cut_for_search(keywords))
+
+        # 数据库Query
+        query = Picture.objects.all()
+
+        # 计算相似度 对结果排序
+        response = [(-1, -1, -1)]
+        for pic in query:
             tags = [tag['name'] for tag in pic.tags.values()]
+            # 重复的元素数量/总数量+1
+            distance = (len(tags+words)-len(set(tags+words)))/(len(set(tags+words))+1)
+            # distance = sqeuclidean(words, tags)
+            temp = [] + response
+            for index, item in enumerate(temp):
+                # import pdb; pdb.set_trace()
+                if distance > item[1]:
+                    response.insert(index, (pic, distance, tags))
 
-            pic_info = dictsub(model_to_dict(pic), ['name', 'source'])
-            pic_info['tags'] = tags
-            response.append(pic_info)
-
-        return HttpResponse(json.dumps(response), content_type="application/json")
+        return HttpResponse(
+            serializer(response[limit*offset:limit*offset+limit]),
+            content_type="application/json"
+        )
