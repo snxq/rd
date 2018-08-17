7091821c6e5296f187ff09a4dc4a31fa83032cc5
yodark <snxq1995@gmail.com>
2018-08-18 00:27:25 +0800

    Fix Repeated Results && arg error

--- Backend/RESTfulAPI/views.py
+++ Backend/RESTfulAPI/views.py
@@ -1,17 +1,18 @@
+# coding: utf-8
+"""RESTful API
+1. Search 图片的搜索
+"""
+
 import json
 
 import jieba
-import pytesseract
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
 from django.views import View
-from PIL import Image
 
 from RESTfulAPI.models import Picture
 from utils import dictsub
 
-import jieba
-from scipy.spatial.distance import sqeuclidean
 # Create your views here.
 
 
@@ -32,25 +33,34 @@ class Search(View):
 
         # 参数处理
         keywords = request.GET['keywords']
-        limit = max(int(request.GET['limit']), 100)
+        limit = int(request.GET['limit']) if int(request.GET['limit']) < 100 else 100
         offset = int(request.GET['offset'])
         words = list(jieba.cut_for_search(keywords))
+        words_length = len(words)
 
         # 数据库Query
         query = Picture.objects.all()
 
         # 计算相似度 对结果排序
-        response = [(-1, -1, -1)]
+        response = []
         for pic in query:
             tags = [tag['name'] for tag in pic.tags.values()]
+            tags_length = len(tags)
+            total_length = len(set(tags+words))
+
             # 重复的元素数量/总数量+1
-            distance = (len(tags+words)-len(set(tags+words)))/(len(set(tags+words))+1)
-            # distance = sqeuclidean(words, tags)
+            distance = (tags_length+words_length-total_length)/(total_length+1)
+
+            # distance == 0 两种情况 1. 没有tags 2. 有但没有重复
+            if distance == 0:
+                response.append((pic, 0, tags))
+                continue
+
             temp = [] + response
             for index, item in enumerate(temp):
-                # import pdb; pdb.set_trace()
-                if distance > item[1]:
+                if not response or (distance > item[1]):
                     response.insert(index, (pic, distance, tags))
+                    break
 
         return HttpResponse(
             serializer(response[limit*offset:limit*offset+limit]),
